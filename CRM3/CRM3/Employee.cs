e159e16924be93e5e0ee5ef91477920bb419657d
Neohack00 <neo@bylock.se>
2019-09-30 13:46:20 +0200

    New class Employee

---
+++ CRM3/CRM3/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM3
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string ID { get; set; }
+
+        public string WorkPlacement { get; set; }
+    }
+}
