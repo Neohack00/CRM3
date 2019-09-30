71c3182ad208774c8c738160b9cd1e38b0a91973
Neohack00 <neo@bylock.se>
2019-09-30 14:34:31 +0200

    Set mode metod implanted and code adepted after mode. New local method updateForm

--- CRM3/CRM3/Form1.Designer.cs
+++ CRM3/CRM3/Form1.Designer.cs
@@ -35,25 +35,30 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(85, 54);
+            this.textBox1.Location = new System.Drawing.Point(85, 70);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(128, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(85, 110);
+            this.textBox2.Location = new System.Drawing.Point(85, 126);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(128, 20);
             this.textBox2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(85, 136);
+            this.button1.Location = new System.Drawing.Point(85, 152);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -63,7 +68,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(296, 51);
+            this.button2.Location = new System.Drawing.Point(439, 70);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(147, 23);
             this.button2.TabIndex = 6;
@@ -74,7 +79,7 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(296, 83);
+            this.listBox1.Location = new System.Drawing.Point(296, 99);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(290, 147);
             this.listBox1.TabIndex = 4;
@@ -82,7 +87,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 57);
+            this.label1.Location = new System.Drawing.Point(26, 73);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 2;
@@ -91,17 +96,69 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 113);
+            this.label2.Location = new System.Drawing.Point(26, 129);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Lastname";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(455, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(552, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(493, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Set/change mode";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(293, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(650, 258);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
@@ -125,6 +182,11 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- CRM3/CRM3/Form1.cs
+++ CRM3/CRM3/Form1.cs
@@ -13,17 +13,32 @@ namespace CRM3
 {
     public partial class Form1 : Form
     {
-        ArrayList MyCustomer = new ArrayList(); 
+        ArrayList MyCustomer = new ArrayList();
+        ArrayList MyEmployee = new ArrayList();
+        private string mode = "Customer" ;
         public Form1()
         {   
             InitializeComponent();
             button1.Text = "Add";
-            button2.Text = "Show Customers";
+            updateForm();
+            button3.Text = "Customer";
+            button4.Text = "Employee";
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MyCustomer.Add(new Customer() { FirstName = textBox1.Text, LastName = textBox2.Text });
+            if (mode == "Customer")
+            {
+                MyCustomer.Add(new Customer() { FirstName = textBox1.Text, LastName = textBox2.Text });
+            }
+            else if (mode == "Employee")
+            {
+                MyEmployee.Add(new Employee() { FirstName = textBox1.Text, LastName = textBox2.Text });
+            }
+
+
+
             textBox1.Clear();
             textBox2.Clear();
             textBox1.Focus();
@@ -33,11 +48,44 @@ namespace CRM3
         private void button2_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
-            foreach (Customer displayedCustomer in MyCustomer)
+            if (mode == "Customer")
             {
-                listBox1.Items.Add(displayedCustomer);
+                foreach (Customer displayedCustomer in MyCustomer)
+                {
+                    listBox1.Items.Add(displayedCustomer);
+                }
+            }
+            else if (mode == "Employee")
+            {
+                foreach (Employee displayedEmployee in MyEmployee)
+                {
+                    listBox1.Items.Add(displayedEmployee);
+                }
             }
 
+
         }
+        //tänker att använder får välja ett typ av lägen de vill vara i genom trycka på en av de två knapperna.
+        //Det kommer finnas två lägen "kund" eller "anställd". Där kund är default läget programet startat med.
+        //Den valda läget kommer ställa om funktionerna så att allt sparas och skrivs ut på rätt sätt
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mode = "Customer";
+            updateForm();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mode = "Employee";
+            updateForm();
+        }
+
+        private void updateForm()
+        {
+            button2.Text = string.Format("Show {0}", mode);
+            label4.Text = string.Format("New {0}", mode);
+            label5.Text = string.Format("{0} list", mode);
+        }
+
     }
 }
